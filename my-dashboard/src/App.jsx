72325cf9f64e2b87f6d924d11559c30b8a42362b
shraddhakakade-18 <shraddhakakade1225@gmail.com>
2025-04-09 11:46:40 +0530

    re-named

--- my-dashboard/src/App.jsx
+++ my-dashboard/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./assets/dashboard.jsx";
+import MenteeDashboard from "./assets/MenteeDashboard.jsx";
 import MentorProfile from "./assets/MentorProfile.jsx";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<MenteeDashboard />} />
         <Route path="/mentor/:id" element={<MentorProfile />} />
         </Routes>
     </Router>
